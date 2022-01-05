83013f75d278238d1a364063597f87c5edddcc60
David Kloster <kloster.davidjonas@gmail.com>
2022-01-05 16:16:32 +0100

    View Change Main

--- NoChromo/Form1.Designer.cs
+++ NoChromo/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 12);
+            this.button1.Location = new System.Drawing.Point(26, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(81, 46);
             this.button1.TabIndex = 0;
             this.button1.Text = "StartYoutube";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,9 +44,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(128, 12);
+            this.button2.Location = new System.Drawing.Point(113, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(81, 45);
             this.button2.TabIndex = 1;
             this.button2.Text = "MuteLoop";
             this.button2.UseVisualStyleBackColor = true;
